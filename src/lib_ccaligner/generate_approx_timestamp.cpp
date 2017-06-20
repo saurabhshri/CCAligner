cb8ca3f4ccf971c307792783c8d699f00557f3c2
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2017-06-21 04:27:56 +0530

    Add basic structure for various output formats.

--- src/lib_ccaligner/generate_approx_timestamp.cpp
+++ src/lib_ccaligner/generate_approx_timestamp.cpp
@@ -5,8 +5,6 @@
 */
 
 #include "generate_approx_timestamp.h"
-#include <fstream>
-
 
 void ms_to_srt_time(long int ms, int *hours, int *minutes, int *seconds, int *milliseconds)
 {
@@ -22,49 +20,30 @@ void ms_to_srt_time(long int ms, int *hours, int *minutes, int *seconds, int *mi
     *hours = (int)ms;
 }
 
-int currentSub::wordNumber;
+std::string extractFileName(std::string fileName)
+{
+    int lastIndex = fileName.find_last_of(".");
+
+    if(lastIndex == std::string::npos)
+        return fileName;
+    else
+        return fileName.substr(0, lastIndex);
+}
+
+int currentSub::_wordNumber;
 
 currentSub::currentSub(SubtitleItem *sub)
 {
     _sub = sub;
-    wordNumber = 0;
-    sentenceLength = _sub->getDialogue().size();
-    wordCount = _sub->getWordCount();
-    dialogueDuration = getDuration(_sub->getStartTime(), _sub->getEndTime());
+    _wordNumber = 0;
+    _sentenceLength = _sub->getDialogue().size();
+    _wordCount = _sub->getWordCount();
+    _dialogueDuration = getDuration(_sub->getStartTime(), _sub->getEndTime());
 
 }
 
-inline int currentSub::getDuration (long startTime, long endTime)
-{
-    if(endTime < startTime)
-    {
-        std::cout << "Error! Incorrect start time and end time of the dialogue.\n";
-        return -1;
-    }
 
-    return endTime - startTime;
-
-}
-
-inline double currentSub::getWordWeight (std::string word)
-{
-    double weight = (double) word.size() / (double) sentenceLength;
-    return weight;
-
-}
-
-void currentSub::assignTime(long int &wordDuration, const std::string &word )
-{
-    if(wordNumber>wordCount)
-        std::cout<<"Oops! Something went wrong!\n";
-
-    double wordWeight = getWordWeight(word);
-    wordDuration = wordWeight * dialogueDuration;
-    wordNumber++;
-
-}
-
-void currentSub::printToSrt(std::string fileName)
+void currentSub::printToSRT(std::string fileName)
 {
     std::ofstream out;
     out.open(fileName, std::ofstream::app);
@@ -85,26 +64,128 @@ void currentSub::printToSrt(std::string fileName)
     }
 }
 
+void currentSub::printToXML(std::string fileName)
+{
+    std::ofstream out;
+    out.open(fileName, std::ofstream::app);
+
+    out<<"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+            "<!DOCTYPE subtitle>\n\n";
+
+    /*
+     * ├───Subtitle
+     *      ├───Start Time
+     *      ├───End Time
+     *      ├───Start Time in ms
+     *      ├───Start Time in ms
+     *      ├───Word
+     *      ├───Style
+     *      |   ├───Style Tag 1
+     *      |   ├───Style Tag 2
+     *      |   └───Style Tag
+     */
+
+    for(int i=0;i<_sub->getWordCount();i++)
+    {
+        out<<"<subtitle>\n";
+        int hh1,mm1,ss1,ms1;
+        int hh2,mm2,ss2,ms2;
+        char printLine[150];
+
+        ms_to_srt_time(_sub->getWordStartTimeByIndex(i),&hh1,&mm1,&ss1,&ms1);
+        ms_to_srt_time(_sub->getWordEndTimeByIndex(i),&hh2,&mm2,&ss2,&ms2);
+
+        sprintf(printLine, "\t<time start=%ld end=%ld></time>\n", _sub->getWordStartTimeByIndex(i), _sub->getWordEndTimeByIndex(i));
+        out<<printLine;
+        sprintf(printLine, "\t<srtTime start=%02d:%02d:%02d,%03d end=%02d:%02d:%02d,%03d></srtTime>\n", hh1, mm1, ss1, ms1, hh2, mm2, ss2, ms2);
+        out<<printLine;
+        out<<"\t<text>";
+        out<<_sub->getWordByIndex(i)<<"</text>\n";
+        out<<"</subtitle>\n\n";
+    }
+}
+
+void currentSub::printToJSON(std::string fileName)
+{
+    std::ofstream out;
+    out.open(fileName, std::ofstream::app);
+
+    for(int i=0;i<_sub->getWordCount();i++)
+    {
+        //print json
+    }
+}
+
+void currentSub::printToConsole(std::string fileName)
+{
+    std::ofstream out;
+    out.open(fileName, std::ofstream::app);
+
+    for(int i=0;i<_sub->getWordCount();i++)
+    {
+        int hh1,mm1,ss1,ms1;
+        int hh2,mm2,ss2,ms2;
+        char timeline[128];
+
+        ms_to_srt_time(_sub->getWordStartTimeByIndex(i),&hh1,&mm1,&ss1,&ms1);
+        ms_to_srt_time(_sub->getWordEndTimeByIndex(i),&hh2,&mm2,&ss2,&ms2);
+
+        sprintf(timeline, "%02d:%02d:%02d,%03d --> %02d:%02d:%02d,%03d\n", hh1, mm1, ss1, ms1, hh2, mm2, ss2, ms2);
+
+        out<<timeline;
+        out<<_sub->getWordByIndex(i)<<"\n\n";
+    }
+}
+
+inline int currentSub::getDuration (long startTime, long endTime)
+{
+    if(endTime < startTime)
+    {
+        std::cout << "Error! Incorrect start time and end time of the dialogue.\n";
+        return -1;
+    }
+
+    return endTime - startTime;
+
+}
+
+inline double currentSub::getWordWeight (std::string word)
+{
+    double weight = (double) word.size() / (double) _sentenceLength;
+    return weight;
+
+}
+
+void currentSub::assignTime(long int &wordDuration, const std::string &word )
+{
+    if(_wordNumber>_wordCount)
+        std::cout<<"Oops! Something went wrong!\n";
+
+    double wordWeight = getWordWeight(word);
+    wordDuration = wordWeight * _dialogueDuration;
+    _wordNumber++;
+
+}
 
 void currentSub::run()
 {
     std::vector<std::string> words = _sub->getIndividualWords();
-    std::vector<long int> wordDuration(wordCount), wordStartTime(wordCount), wordEndTime(wordCount);
+    std::vector<long int> wordDuration(_wordCount), wordStartTime(_wordCount), wordEndTime(_wordCount);
     int totalWordDuration = 0;
 
-    for(int i = 0; i < wordCount; i++)
+    for(int i = 0; i < _wordCount; i++)
     {
         assignTime(wordDuration[i], words[i]);
         totalWordDuration+=wordDuration[i];
     }
 
-    int silenceDuration = (wordCount!=1)?((dialogueDuration - totalWordDuration) / (wordCount - 1)):0;
+    int silenceDuration = (_wordCount!=1)?((_dialogueDuration - totalWordDuration) / (_wordCount - 1)):0;
     long int prevTime = _sub->getStartTime();
 
 
-    for(int i = 0; i < wordCount; i++)
+    for(int i = 0; i < _wordCount; i++)
     {
-        if(i == wordCount - 1)
+        if(i == _wordCount - 1)
             silenceDuration = 0;
 
         wordStartTime[i] = prevTime;
@@ -118,15 +199,21 @@ void currentSub::run()
 currentSub::~currentSub()
 {
     _sub = NULL;
-    wordNumber = 0;
-    sentenceLength = 0;
-    wordCount = 0;
-    dialogueDuration = 0;
+    _wordNumber = 0;
+    _sentenceLength = 0;
+    _wordCount = 0;
+    _dialogueDuration = 0;
 }
 
-ApproxAligner::ApproxAligner(std::string fileName)
+ApproxAligner::ApproxAligner(std::string fileName, outputFormats outputFormat)
 {
     _fileName = fileName;
+    _outputFormat = outputFormat;
+    _outputFileName = extractFileName(_fileName);
+
+    std::ofstream out;
+    out.open(_outputFileName);
+    out.close();
 }
 
 void ApproxAligner::align()
@@ -139,7 +226,25 @@ void ApproxAligner::align()
     {
         currentSub * currSub = new currentSub(sub);
         currSub->run();
-        currSub->printToSrt("ElonMusk.srt");
+
+        switch (_outputFormat)
+        {
+            case srt:   currSub->printToSRT(_outputFileName);
+                        break;
+
+            case xml:   currSub->printToXML(_outputFileName);
+                        break;
+
+            case json:  currSub->printToJSON(_outputFileName);
+                        break;
+
+            case console: currSub->printToConsole(_outputFileName);
+                        break;
+
+            default: std::cout<<"An error occurred while choosing output format!";
+                        exit(2);
+        }
+
         delete currSub;
     }
 
--- src/lib_ccaligner/generate_approx_timestamp.h
+++ src/lib_ccaligner/generate_approx_timestamp.h
@@ -9,11 +9,20 @@
 
 #include "srtparser.h"
 
+enum outputFormats
+{
+    srt,
+    xml,
+    json,
+    console,
+    blank
+};
+
 class currentSub
 {
-    int sentenceLength, wordCount;
-    long dialogueDuration;
-    static int wordNumber;
+    int _sentenceLength, _wordCount;
+    long _dialogueDuration;
+    static int _wordNumber;
     SubtitleItem *_sub;
 
 
@@ -22,7 +31,10 @@ public:
     double getWordWeight (std::string word);
     currentSub(SubtitleItem *sub);
     void run();
-    void printToSrt(std::string fileName);
+    void printToSRT(std::string fileName);
+    void printToJSON(std::string fileName);
+    void printToXML(std::string fileName);
+    void printToConsole(std::string fileName);
     void assignTime(long int &wordDuration, const std::string &word );
     ~currentSub();
 };
@@ -30,11 +42,15 @@ public:
 class ApproxAligner
 {
 private:
-    std::string _fileName;
+    std::string _fileName,_outputFileName;
+    outputFormats _outputFormat;
 public:
-    ApproxAligner(std::string fileName);
+    ApproxAligner(std::string fileName, outputFormats outputFormat = srt);
     void align();
     ~ApproxAligner();
 };
+
+void ms_to_srt_time(long int ms, int *hours, int *minutes, int *seconds, int *milliseconds);
+std::string extractFileName(std::string fileName);
 
 #endif //GENERATE_APPROX_TIMESTAMP_H
