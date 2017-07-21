3550a5cff64722a693c11d6891b3d2f53d4ac7e2
Saurabh Shrivastava <saurabh.shrivastava54@gmail.com>
2017-07-21 18:58:59 +0530

    Added some installation scripts for basic dependencies.
    I would recommend to install them manually.

---
+++ install/install_grammar_tools.sh
@@ -0,0 +1,31 @@
+echo "You must be root to install grammar tool dependencies. Alterntively, you may install them manually." 2>&1
+echo "Following dependencies must be met for generating grammars and language models : "
+echo "CMUCLMTK : https://sourceforge.net/projects/cmusphinx/files/cmuclmtk/0.7/"
+echo "g2p-seq2seq : https://github.com/cmusphinx/g2p-seq2seq"
+
+read -p "Press Enter to continue, ctrl + c to quit."
+
+echo "Installing CMUCLMTK - For Creating Language Models on the fly,"
+echo "you may alternatively use the web service to generate the same."
+cd dependencies/
+echo "Uncompressing toolkits : "
+tar xvpzf cmuclmtk-0.7.tar.gz
+unzip g2p-seq2seq-master.zip
+cd cmuclmtk-0.7
+./configure
+make
+sudo make install
+if [ $? -ne 0 ]; then
+    exit
+echo "Installing Tensorflow : "
+sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.0-cp27-none-linux_x86_64.whl
+if [ $? -ne 0 ]; then
+    exit
+echo "Installing g2p-seq2seq - For Creating dictionaries"
+cd ../g2p-seq2seq-master/
+sudo python setup.py install
+if [ $? -ne 0 ]; then
+    exit
+cd ../../
+cp -rf ../src/lib_ext/model .
+cp -rf ../src/lib_ext/g2p-seq2seq-cmudict .
